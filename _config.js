b11ad4be03fdb8e4a129728f3b71efac2a83bfd4
Becky <beckychellah080@gmail.com>
2022-03-24 18:27:13 +0300

    sorted the DB issue

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<Becky>:<GodIsFaithful2020>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<Becky>:<GodIsFaithful2020>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<Becky>:<GodIsFaithful2020>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Becky:<password>@week5ipcluster.mfxxe.mongodb.net/Week5IPCluster?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Becky:<password>@week5ipcluster.mfxxe.mongodb.net/Week5IPCluster?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Becky:<password>@week5ipcluster.mfxxe.mongodb.net/Week5IPCluster?retryWrites=true&w=majority',
 }
 module.exports = config;
