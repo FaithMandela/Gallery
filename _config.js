bca12fabbc9296d01e88d3ef2c9cbd20703b949d
Becky <beckychellah080@gmail.com>
2022-03-24 19:01:19 +0300

    changed test config

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Becky:GodIsFaithful2020@week5ipcluster.mfxxe.mongodb.net/week5IPCluster?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Becky:GodIsFaithful2020@week5ipcluster.mfxxe.mongodb.net/week5IPCluster-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Becky:GodIsFaithful2020@week5ipcluster.mfxxe.mongodb.net/week5IPCluster?retryWrites=true&w=majority',
 }
 module.exports = config;
